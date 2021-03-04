91b26bdc65b7d63b8a64ee9ae66ce690d8f5ba21
Alkamyan David <ada_170301@mail.ru>
2021-03-04 14:47:00 +0500

    Minor changes

--- payme_billing/admin.py
+++ payme_billing/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import PaymeTransaction
 
 # Register your models here.
+
+@admin.register(PaymeTransaction)
+class PaymeTransactionAdmin(admin.ModelAdmin):
+    readonly_fields = super().fields
