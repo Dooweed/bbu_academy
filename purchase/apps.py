5a0053a6bd65628f1e588adf9ee2213ca02a16f3
Alkamyan David <ada_170301@mail.ru>
2021-03-17 19:33:30 +0500

    Minor changes & Bug fixes

--- purchase/apps.py
+++ purchase/apps.py
@@ -5,10 +5,11 @@ class PurchaseConfig(AppConfig):
     name = 'purchase'
 
     def ready(self):
-        from purchase.models import PurchaseRecord, Student, IndividualPayer
-        from django.db.models.signals import pre_delete
-        from purchase.signals.signals import temp_files_delete
-
-        pre_delete.connect(temp_files_delete, sender=PurchaseRecord)
-        pre_delete.connect(temp_files_delete, sender=Student)
-        pre_delete.connect(temp_files_delete, sender=IndividualPayer)
+        pass
+        # from purchase.models import PurchaseRecord, Student, IndividualPayer
+        # from django.db.models.signals import pre_delete
+        # from purchase.signals.signals import temp_files_delete
+        #
+        # pre_delete.connect(temp_files_delete, sender=PurchaseRecord)
+        # pre_delete.connect(temp_files_delete, sender=Student)
+        # pre_delete.connect(temp_files_delete, sender=IndividualPayer)
--- purchase/models.py
+++ purchase/models.py
@@ -235,9 +235,6 @@ class PurchaseRecord(PaymeStateMixin):
     def __str__(self):
         return f"Покупка - {self.payer_name()}"
 
-    def __del__(self):
-        self.delete_temp_files()
-
     def get_amount(self):
         return self.overall_price
 
