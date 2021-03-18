0ba710f150e7b49ece67876d300cac89ecb54ab8
Alkamyan David <ada_170301@mail.ru>
2021-03-18 18:32:58 +0500

    Minor changes & Bug fixes

--- payme_billing/utils.py
+++ payme_billing/utils.py
@@ -7,5 +7,6 @@ def get_payment_link(purchase_id, phone, amount, lang, return_url=None):
     params = f"m={WEB_CASH_ID};ac.purchase_id={purchase_id};ac.phone={phone};amount={amount};lang={lang}"
     if return_url:
         params += f"c={return_url}"
-    url = f"{URL}/{b64encode(params.encode())}"
+    params_bytes = params.encode('utf-8')
+    url = f"{URL}/{b64encode(params_bytes)}"
     return url
