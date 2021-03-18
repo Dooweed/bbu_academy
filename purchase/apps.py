da8e35e74538dcf6823580e17ebae849da2e3191
Alkamyan David <ada_170301@mail.ru>
2021-03-18 16:27:54 +0500

    Minor changes & Bug fixes

--- purchase/apps.py
+++ purchase/apps.py
@@ -5,11 +5,10 @@ class PurchaseConfig(AppConfig):
     name = 'purchase'
 
     def ready(self):
-        pass
-        # from purchase.models import PurchaseRecord, Student, IndividualPayer
-        # from django.db.models.signals import pre_delete
-        # from purchase.signals.signals import temp_files_delete
-        #
-        # pre_delete.connect(temp_files_delete, sender=PurchaseRecord)
-        # pre_delete.connect(temp_files_delete, sender=Student)
-        # pre_delete.connect(temp_files_delete, sender=IndividualPayer)
+        from purchase.models import PurchaseRecord, Student, IndividualPayer
+        from django.db.models.signals import pre_delete
+        from purchase.signals.signals import temp_files_delete
+
+        pre_delete.connect(temp_files_delete, sender=PurchaseRecord)
+        pre_delete.connect(temp_files_delete, sender=Student)
+        pre_delete.connect(temp_files_delete, sender=IndividualPayer)
