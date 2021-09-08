24b207ddc620db9a948c7c7c9e0132e8b34a09b1
Alkamyan David <ada_170301@mail.ru>
2021-09-08 21:44:33 +0500

    Added support for pinfl, inn now is secondary

--- certificates/urls.py
+++ certificates/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.registry_view, name='registry'),
     path('certificate', views.registry_view, name='certificate'),
     path('qr_code/<int:inn>', views.qr_code, name="qr_code"),
-    path('download_pdf_certificate/<int:inn>', views.download_pdf_certificate, name="download_pdf_certificate"),
+    path('download_pdf_certificate/<int:inn_or_pinfl>', views.download_pdf_certificate, name="download_pdf_certificate"),
 ]
 
