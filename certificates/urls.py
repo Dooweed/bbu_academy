1228170add8eb6365f799fd2c43d4e937edffc82
Alkamyan David <ada_170301@mail.ru>
2021-11-01 02:59:24 +0500

    Minor fixes

--- certificates/urls.py
+++ certificates/urls.py
@@ -7,7 +7,6 @@ app_name = "registry"
 urlpatterns = [
     path('', views.registry_view, name='registry'),
     path('certificate', views.registry_view, name='certificate'),
-    path('qr_code/<int:inn>', views.qr_code, name="qr_code"),
+    path('qr_code/<int:inn_or_pinfl>', views.qr_code, name="qr_code"),
     path('download_pdf_certificate/<int:inn_or_pinfl>', views.download_pdf_certificate, name="download_pdf_certificate"),
 ]
-
