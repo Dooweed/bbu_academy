13915d3b75f4474e33b4a6b88c6f7c9e09d5f6fc
Alkamyan David <ada_170301@mail.ru>
2021-12-27 00:56:42 +0500

    New apps "services" and "small_purchase" - New section and New purchase type, respectively

--- payme_billing/admin.py
+++ payme_billing/admin.py
@@ -6,5 +6,8 @@ from .vars.settings import ADMIN
 if ADMIN:
     @admin.register(PaymeTransaction)
     class PaymeTransactionAdmin(admin.ModelAdmin):
+        list_display = ("transaction_id", "record_id", "phone")
+        list_filter = ("state",)
+        search_fields = ("transaction_id", "record_id")
         fields = ("transaction_id", "state", "record_id", "phone", "amount", "payme_creation_time", "creation_time", "perform_time", "cancel_time", "denial_reason")
         readonly_fields = ("record_id", "transaction_id", "amount", "creation_time", "perform_time", "cancel_time", "payme_creation_time", "denial_reason", "phone")
